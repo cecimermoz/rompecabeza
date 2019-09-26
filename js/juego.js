868c39df442d032d3d864c358792a66bea396fe3
Cecilia Mermoz <42853540+cecimermoz@users.noreply.github.com>
2019-09-25 22:03:15 -0300

    funciona función de "Intercambiar fichas"

--- js/juego.js
+++ js/juego.js
@@ -15,6 +15,14 @@ var grilla = [
     [7, 8, 9]
 ];
 
+// ESTA ES LA QUE DEBE SER MODIFICADA
+
+var auxiliar = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
 /* Estas dos variables son para guardar la posición de la pieza vacía. 
 Esta posición comienza siendo la [2, 2]*/
 var filaVacia = 2;
@@ -40,60 +48,26 @@ function direccion (nuevaDireccion){
 
 /* Esta función va a chequear si el Rompecabezas esta en la posicion ganadora. 
 Existen diferentes formas de hacer este chequeo a partir de la grilla. */
-function recorrerSimple(nombreArray) {
-  for(var i = 0; i < nombreArray.length; i++){
-    return nombreArray[i];
-  }
-}
-function recorrerDoble(nombreArray) {
-  for(var i = 0; i < nombreArray.length; i++){
-    for(var j = 0; j < nueva[i].length; j++){
-      return nombreArray[i][j];
-      // No puede ser un return porque sino se corta el bucle, tengo que solucionar de otro modo
-    }
-  }
-}
-function comparar(a, b){
-  if(a == b){
-    return true;
-  } else {
-    return false;
-  }
-}
 
-/* con la Grilla del tp y
-var nueva = [
-    [2, 1, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-comparar me da true, ¿por qué? */
-
-comparar(recorrerDoble(grilla), recorrerDoble(nueva));
-
-// Funciona pero si no hay correlatividad, no consologuea esos numeros, pero no me tira error
-// hay que arreglar.
-function chequearSiGano(nueva, grilla) {
-  for(var i = 0; i < nueva.length; i++){
-    if (nueva[i][j] == grilla[i][j]){
-      for(var j = 0; j < nueva[i].length && nueva[i][j] == grilla[i][j]; j++){
-        console.log(nueva[i][j] + " es igual a lo que hay en la grilla: " + grilla[i][j])
+function chequearSiGano() {
+  var bool = true;
+  for(var i = 0; i < grilla.length; i++){
+    for(var j = 0;j < grilla[i].length; j++){
+      if(!grilla[i][j]){
+        bool = false;
       }
-    } else {
-      console.log("Error")
     }
   }
+  return bool;
 }
-chequearSiGano(nueva, grilla);
-
-
-
-
 
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
-    alert("Felicitaciones, ganaste");
+  if(chequearSiGano()){
+    alert("Felicitaciones, ganaste weon");
+  }
 }
+mostrarCartelGanador();
 
 /* Función que intercambia dos posiciones en la grilla.
 Pensar como intercambiar dos posiciones en un arreglo de arreglos. 
@@ -107,7 +81,9 @@ en ambas posiciones el mismo valor.
 Se te ocurre cómo solucionar esto con una variable temporal?
 */
 function intercambiarPosicionesGrilla(filaPos1, columnaPos1, filaPos2, columnaPos2) {
-    //COMPLETAR
+    var aux = grilla[filaPos1][columnaPos1];
+    grilla[filaPos1][columnaPos1] = grilla[filaPos2][columnaPos2];
+    grilla[filaPos2][columnaPos2] = aux;
 }
 
 // Actualiza la posición de la pieza vacía
@@ -302,3 +278,7 @@ function iniciar() {
 
 // Ejecutamos la función iniciar
 iniciar();
+
+
+
+
